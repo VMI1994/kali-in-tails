ec58021b3bba2495166bd158d0cdfe7b667ea1f8
Just Playin <ensminger@yahoo.com>
2025-09-28 10:18:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+clear
+echo "Updating system"
 apt update -y
-apt upgrade -yq
+clear
+echo "Installing Docker"
 git clone https://github.com/vmi1994/docker-portainer
 cd docker-portainer
 bash Install.sh
-docker build -t kali .
-docker run -it -rm --name kali kali
+clear
+echo "updating torrc"
+sudo echo "ExitNodes {ru}" >> /etc/tor/torrc
+sudo echo "StrictNodes 1" >> /etc/tor/torrc
+clear
+echo "Start Tor and then hit enter"
+read junk
+clear
+echo "building the kali docker image"
+sudo docker build -t kali .
+clear
+echo "Starting kali linux"
+sudo docker run -it -rm --name kali kali
 exit
 
